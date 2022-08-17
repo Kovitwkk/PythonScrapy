026a0d8c312003367ea2242e551f497c88d1c500
Kovitwk <180169g@mymail.nyp.edu.sg>
2022-08-17 17:00:37 +0800

    First Commit

---
+++ ScrapyTest/ScrapyTest/spiders/SpiderTesting.py
@@ -0,0 +1,17 @@
+import scrapy
+
+class WhiskeySpider(scrapy.Spider):
+    name = 'whiskey'
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response):
+        title = response.css('title::text')[0].extract()
+        quotes = response.css('span.text::text').extract()
+        author = response.css('.author::text').extract()
+        tags = response.css('.tag::text').extract()
+        yield {
+            'titletext' : title,
+            'quotes' : quotes,
+            'author' : author,
+            'tags' : tags
+        }
